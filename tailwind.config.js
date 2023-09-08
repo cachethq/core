0ddd23e09a96f435f2b9c50f63260fdcb51b6554
James Brooks <james@alt-three.com>
2023-09-08 09:03:18 +0100

    Add primary colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      //
+      colors: {
+        primary: {
+          '50': '#e6f7ed',
+          '100': '#c1edce',
+          '200': '#9be3b1',
+          '300': '#74d994',
+          '400': '#4dce76',
+          '500': '#04c147', // Base Color
+          '600': '#03ad40',
+          '700': '#038f34',
+          '800': '#027227',
+          '900': '#01561a',
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
