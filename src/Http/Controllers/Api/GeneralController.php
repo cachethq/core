f61aed0a30b42414c7d41e8d82dc99f66ad8cfaf
James Brooks <james@alt-three.com>
2023-09-13 20:14:15 +0100

    Add general API routes

---
+++ src/Http/Controllers/Api/GeneralController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Cachet\Http\Controllers\Api;
+
+use Cachet\Cachet;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
+
+class GeneralController extends Controller
+{
+    public function ping(): JsonResponse
+    {
+        return response()->json(['data' => 'Pong!']);
+    }
+
+    public function version(): JsonResponse
+    {
+        return response()->json([
+            'data' => [
+                'version' => Cachet::version(),
+            ],
+        ]);
+    }
+
+    public function status(): JsonResponse
+    {
+        return response()->json([
+            'data' => [
+//                'status' => Cachet::status(),
+//                'message' => Cachet::statusMessage(),
+            ],
+        ]);
+    }
+}
