0b3442413892d837458087b91ba48fe6d5a70832
James Brooks <james@alt-three.com>
2023-09-13 19:42:57 +0100

    Fire the `ComponentStatusWasChanged` event when a component's status is changed

--- src/Actions/Component/UpdateComponent.php
+++ src/Actions/Component/UpdateComponent.php
@@ -2,6 +2,7 @@
 
 namespace Cachet\Actions\Component;
 
+use Cachet\Events\Components\ComponentStatusWasChanged;
 use Cachet\Models\Component;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -11,7 +12,13 @@ class UpdateComponent
 
     public function handle(Component $component, array $data): Component
     {
+        $oldStatus = $component->status;
+
         $component->update($data);
+
+        if ($component->wasChanged('status')) {
+            ComponentStatusWasChanged::dispatch($component, $oldStatus, $component->status);
+        }
 
         return $component->fresh();
     }
---
+++ src/Events/Components/ComponentStatusWasChanged.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cachet\Events\Components;
+
+use Cachet\Enums\ComponentStatusEnum;
+use Cachet\Models\Component;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ComponentStatusWasChanged
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Component $component, public ComponentStatusEnum $oldStatus, public ComponentStatusEnum $newStatus)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
