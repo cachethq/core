ba91c48ed7eecbe4b5297a946742b97618a705e2
James Brooks <james@alt-three.com>
2023-09-13 19:46:59 +0100

    Fire `ComponentStatusWasChanged` event when changing component status (#19)

--- tests/Unit/Actions/Component/UpdateComponentTest.php
+++ tests/Unit/Actions/Component/UpdateComponentTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Cachet\Actions\Component\UpdateComponent;
+use Cachet\Enums\ComponentStatusEnum;
+use Cachet\Events\Components\ComponentStatusWasChanged;
 use Cachet\Events\Components\ComponentUpdated;
 use Cachet\Models\Component;
 use Illuminate\Support\Facades\Event;
@@ -28,4 +30,25 @@ it('can update a component', function () {
         ->description->toBe($data['description']);
 
     Event::assertDispatched(ComponentUpdated::class, fn ($event) => $event->component->is($component));
+    Event::assertNotDispatched(ComponentStatusWasChanged::class);
+});
+
+it('dispatches ComponentStatusWasChanged when the status is changed', function () {
+    Event::fake();
+
+    $component = Component::factory()->create([
+        'name' => 'My Component',
+        'description' => 'My component description.',
+        'status' => ComponentStatusEnum::operational,
+    ]);
+
+    UpdateComponent::run($component, [
+        'status' => ComponentStatusEnum::major_outage,
+    ]);
+
+    Event::assertDispatched(ComponentStatusWasChanged::class, function (ComponentStatusWasChanged $event) use ($component) {
+        return $event->component->is($component) &&
+            $event->oldStatus === ComponentStatusEnum::operational &&
+            $event->newStatus === ComponentStatusEnum::major_outage;
+    });
 });
