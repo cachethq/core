f3986746e73e648bbf0f25bef0caab0fbee53e4f
James Brooks <james@alt-three.com>
2024-01-17 20:24:19 +0000

    Fix badge in dark mode

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -7,7 +7,7 @@
 'unknown' => 'bg-blue-200',
 ][$type]
 }} flex items-center gap-1 rounded-full px-2 py-1 text-sm font-semibold leading-tight">
-    @svg('cachet-component-'.$type, 'h-6 w-6')
+    @svg('cachet-component-'.$type, 'h-6 w-6 dark:text-zinc-700')
 
     <div class="dark:text-zinc-700">{{ str($type)->title() }}</div>
 </div>
