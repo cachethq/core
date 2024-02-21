a908f3a0b7531e327629a20b7d7147287c847ad2
James Brooks <james@alt-three.com>
2024-02-21 21:09:48 +0000

    wip

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,4 +1,4 @@
-@if ($about)
+@if ($about !== '')
 <div>
     <h2 class="text-3xl font-semibold">{{ __('About This Site') }}</h2>
     <div class="prose-sm md:prose prose-zinc mt-1 dark:prose-invert prose-a:text-primary-500 prose-a:underline">
