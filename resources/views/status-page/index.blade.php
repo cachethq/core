a90eddcb7cc6960ee190c94a071d4c4cfbba91ba
James Brooks <james@alt-three.com>
2024-02-22 10:45:44 +0000

    Move status bar to top

--- resources/views/status-page/index.blade.php
+++ resources/views/status-page/index.blade.php
@@ -2,9 +2,9 @@
     <x-cachet::header />
 
     <div class="container mx-auto max-w-5xl px-4 py-10 sm:px-6 lg:px-8 flex flex-col space-y-6">
-        <x-cachet::about />
-
         <x-cachet::status-bar />
+
+        <x-cachet::about />
 
         @foreach($componentGroups as $componentGroup)
         <x-cachet::component-group :component-group="$componentGroup"/>
