a75a514500e746755311e3c447540e87c24cb3d7
James Brooks <james@alt-three.com>
2024-02-27 16:56:44 +0000

    Incident pagination

--- resources/views/components/incidents.blade.php
+++ resources/views/components/incidents.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-6">
+<div class="flex flex-col gap-8">
     <div class="border-b py-2 dark:border-zinc-700">
         <h2 class="text-2xl font-semibold">Past Incidents</h2>
     </div>
@@ -7,5 +7,15 @@
         @foreach ($incidents as $date => $incident)
         <x-cachet::incident :date="$date" :incidents="$incident" />
         @endforeach
+    </div>
+
+    <div class="flex justify-between">
+        <a href="{{ route('cachet.status-page', ['from' => $from]) }}" class="text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
+            {{ __('Previous Incidents') }}
+        </a>
+
+        <a href="{{ route('cachet.status-page', ['from' => $to]) }}" class="text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
+            {{ __('Newer Incidents') }}
+        </a>
     </div>
 </div>
