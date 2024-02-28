65c97edd2c4db43f68f6a6381565ffbfe3f103dc
James Brooks <james@alt-three.com>
2024-02-28 12:55:28 +0000

    Display incident dates

--- resources/views/components/incidents.blade.php
+++ resources/views/components/incidents.blade.php
@@ -1,6 +1,11 @@
 <div class="flex flex-col gap-8">
-    <div class="border-b py-2 dark:border-zinc-700">
-        <h2 class="text-2xl font-semibold">{{ __('Past Incidents') }}</h2>
+    <div class="border-b py-2 dark:border-zinc-700 flex justify-between flex-col md:flex-row md:items-center">
+        <div>
+            <h2 class="text-2xl font-semibold">{{ __('Past Incidents') }}</h2>
+        </div>
+        <div class="text-sm text-zinc-500 dark:text-zinc-400">
+            {{ __('Showing :from - :to', ['from' => $from, 'to' => $to]) }}
+        </div>
     </div>
 
     <div class="flex flex-col gap-14 w-full">
