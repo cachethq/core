38c138a91e2c7a79dfa9d3c9669cbcc0e9764083
James Brooks <james@alt-three.com>
2024-02-28 12:50:55 +0000

    Handle only disrupted days

--- resources/views/components/incidents.blade.php
+++ resources/views/components/incidents.blade.php
@@ -1,12 +1,16 @@
 <div class="flex flex-col gap-8">
     <div class="border-b py-2 dark:border-zinc-700">
-        <h2 class="text-2xl font-semibold">Past Incidents</h2>
+        <h2 class="text-2xl font-semibold">{{ __('Past Incidents') }}</h2>
     </div>
 
     <div class="flex flex-col gap-14 w-full">
-        @foreach ($incidents as $date => $incident)
+        @forelse ($incidents as $date => $incident)
         <x-cachet::incident :date="$date" :incidents="$incident" />
-        @endforeach
+        @empty
+        <div class="text-zinc-500 dark:text-zinc-400 text-center">
+            {{ __('No incidents reported between :from and :to.', ['from' => $from, 'to' => $to]) }}
+        </div>
+        @endforelse
     </div>
 
     <div class="flex justify-between">
