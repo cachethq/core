3a3f6b64b50b51f715e1fbb7b199e7b5f789df67
James Brooks <james@alt-three.com>
2024-03-05 17:08:08 +0000

    Fix brand logo

--- src/CachetDashboardServiceProvider.php
+++ src/CachetDashboardServiceProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class CachetDashboardServiceProvider extends PanelProvider
