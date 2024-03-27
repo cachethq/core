7cc1ccd3bccbe37d2a7101b479627afc1a4ad25c
James Brooks <james@alt-three.com>
2024-03-27 10:16:45 +0000

    Update sessions migration

--- database/migrations/2016_04_05_142933_create_sessions_table.php
+++ database/migrations/2016_04_05_142933_create_sessions_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->unique();
-            $table->integer('user_id')->nullable();
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
-            $table->text('payload');
-            $table->integer('last_activity');
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
