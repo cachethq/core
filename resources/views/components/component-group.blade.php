e6eaae044df03162b323dd7259e2506a75af52e6
James Brooks <james@alt-three.com>
2024-09-08 16:04:07 +0100

    Improve pluralization

--- resources/views/components/component-group.blade.php
+++ resources/views/components/component-group.blade.php
@@ -14,9 +14,11 @@
             <x-heroicon-o-chevron-up ::class="!$disclosure.isOpen && 'rotate-180'" class="h-6 w-6 transition" />
         </button>
 
+        @if(($incidentCount = $componentGroup->components->sum('incidents_count')) > 0)
         <a href="#" class="rounded border border-zinc-800 px-2 py-1 text-xs font-semibold text-zinc-800 dark:border-zinc-600 dark:text-zinc-400">
-            {{ __(':incidents Incidents', ['incidents' => $componentGroup->components->sum('incidents_count')]) }}
+            {{ trans_choice(':incidents Incidents', $incidentCount, ['incidents' => $incidentCount]) }}
         </a>
+        @endif
     </div>
 
     <div x-disclosure:panel x-collapse class="flex flex-col divide-y bg-white dark:bg-zinc-800">
