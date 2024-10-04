a43b8fe7a7f20003cf4f5cf9c73e6e9db3f36a21
Di <scharf.diana@googlemail.com>
2024-10-04 12:26:36 +0200

    Updated styling of text color in header, schedule and incident components for dark mode

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,11 +12,11 @@
 
     @if ($dashboardLoginLink)
     <div class="flex items-center gap-2.5 sm:gap-5">
-        <a href="{{ Cachet\Cachet::dashboardPath() }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-white dark:text-zinc-500 dark:hover:text-zinc-300 sm:text-base">{{ __('Dashboard') }}</a>
+        <a href="{{ Cachet\Cachet::dashboardPath() }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 sm:text-base">{{ __('Dashboard') }}</a>
         {{-- Condition Button. --}}
-{{--        <a href="{{ route('cachet.dashboard.index') }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-white dark:text-zinc-500 dark:hover:text-zinc-300 sm:text-base">Logout</a>--}}
+{{--        <a href="{{ route('cachet.dashboard.index') }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 sm:text-base">Logout</a>--}}
         {{-- Conditional Button. --}}
-{{--        <a href="{{ route('cachet.dashboard.index') }}" class="rounded bg-primary-500 px-2 py-1 text-sm font-semibold text-zinc-800 transition hover:bg-primary-600">Subscribers</a>--}}
+{{--        <a href="{{ route('cachet.dashboard.index') }}" class="rounded bg-primary-500 px-2 py-1 text-sm font-semibold text-zinc-800 dark:text-zinc-400 transition hover:bg-primary-600">Subscribers</a>--}}
     </div>
     @endif
 </div>
