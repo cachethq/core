2437e178ffa6c462f30a7f0481ad664bd769dd1d
James Brooks <james@alt-three.com>
2024-10-04 09:48:30 +0100

    Configure Workbench to load Cachet from `/`

--- workbench/config/cachet.php
+++ workbench/config/cachet.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | The User Model.
+     |--------------------------------------------------------------------------
+     |
+     | This is the model that will be used to authenticate users. This model
+     | must be an instance of Illuminate\Foundation\Auth\User.
+     */
     'user_model' => Workbench\App\User::class,
+
 ];
