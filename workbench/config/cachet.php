92f0705609979b81700e9ab4608e143e56f89116
James Brooks <james@alt-three.com>
2024-10-04 09:59:04 +0100

    Revert Testbench changes

--- workbench/config/cachet.php
+++ workbench/config/cachet.php
@@ -1,15 +1,5 @@
 <?php
 
 return [
-
-    /*
-     |--------------------------------------------------------------------------
-     | The User Model.
-     |--------------------------------------------------------------------------
-     |
-     | This is the model that will be used to authenticate users. This model
-     | must be an instance of Illuminate\Foundation\Auth\User.
-     */
     'user_model' => Workbench\App\User::class,
-
 ];
