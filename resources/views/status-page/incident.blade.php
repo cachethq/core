4a1db5bec1db91cf68b7b276ca9a716ef9be909c
James Brooks <james@alt-three.com>
2024-10-04 13:04:01 +0100

    Remove About panel from incident view

--- resources/views/status-page/incident.blade.php
+++ resources/views/status-page/incident.blade.php
@@ -4,8 +4,6 @@
     <div class="container mx-auto max-w-5xl px-4 py-10 sm:px-6 lg:px-8 flex flex-col space-y-6">
         <x-cachet::status-bar />
 
-        <x-cachet::about />
-
         <div class="flex flex-col gap-6">
             <div class="flex flex-col gap-14 w-full">
                 <x-cachet::incident :date="$incident->timestamp" :incidents="[$incident]" />
