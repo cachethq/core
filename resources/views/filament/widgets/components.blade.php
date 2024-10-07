9f8051ca5779853b73e4bacb2d9eb00255e55007
Alex Williams <alex@jump24.co.uk>
2024-10-07 15:16:27 +0100

    Initial work with component manager

--- resources/views/filament/widgets/components.blade.php
+++ resources/views/filament/widgets/components.blade.php
@@ -1,7 +1,10 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="flex flex-col items-center justify-center">
-            <h1 class="text-lg font-semibold">Component Manager</h1>
+            <h1 class="text-lg font-semibold mb-4">Component Manager</h1>
+            <form>
+                {{ $this->form }}
+            </form>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
--- src/Filament/Widgets/Components.php
+++ src/Filament/Widgets/Components.php
@@ -2,11 +2,72 @@
 
 namespace Cachet\Filament\Widgets;
 
+use Cachet\Enums\ComponentStatusEnum;
+use Cachet\Models\Component;
+use Cachet\Models\ComponentGroup;
+use Filament\Forms\Components\Component as FilamentFormComponent;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Widgets\Widget;
+use Illuminate\Support\Collection;
 
-class Components extends Widget
+class Components extends Widget implements HasForms
 {
+    use InteractsWithForms;
+
     protected static string $view = 'cachet::filament.widgets.components';
 
     protected int|string|array $columnSpan = 'full';
+
+    public Collection $formData;
+
+    public function mount(): void
+    {
+        $this->formData = ComponentGroup::query()
+            ->select(['id'])
+            ->where('visible', '=', true)
+            ->with('components:id,component_group_id,status')
+            ->get()
+            ->mapWithKeys(function (ComponentGroup $componentGroup) {
+                $components = $componentGroup->components->mapWithKeys(function (Component $component) {
+                    return [$component->id => $component->only('status')];
+                });
+                return [$componentGroup->id => ['components' => $components]];
+            });
+    }
+
+    public function form(Form $form): Form
+    {
+        $schema = ComponentGroup::query()
+            ->select(['id', 'name', 'collapsed'])
+            ->where('visible', '=', true)
+            ->with('components:id,component_group_id,status')
+            ->get()
+            ->map(function (ComponentGroup $componentGroup): FilamentFormComponent {
+                return Section::make($componentGroup->name)
+                    ->schema(function () use ($componentGroup) {
+                        return
+                            $componentGroup->components->map(function (Component $component) use ($componentGroup) {
+                                return
+                                    Group::make([
+                                        Radio::make($componentGroup->id . '.components.' . $component->id . '.status')
+                                            ->label($component->name)
+                                            ->columns(count(ComponentStatusEnum::cases()))
+                                            ->live()
+                                            ->options(ComponentStatusEnum::class)
+                                            ->afterStateUpdated(function (ComponentStatusEnum $state) use ($component) {
+                                                return $component->update(['status' => $state]);
+                                            })
+                                    ]);
+                            })->toArray();
+                    })
+                    ->collapsed($componentGroup->isCollapsible());
+            })->toArray();
+
+        return $form->schema($schema)->statePath('formData');
+    }
 }
