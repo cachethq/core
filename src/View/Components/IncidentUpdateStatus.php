cb9a873b148b6d379b144bba92f2fd7be5017e56
James Brooks <james@alt-three.com>
2024-10-07 11:51:16 +0100

    Improve incident timeline view with no updates

--- src/View/Components/IncidentUpdateStatus.php
+++ src/View/Components/IncidentUpdateStatus.php
@@ -2,6 +2,7 @@
 
 namespace Cachet\View\Components;
 
+use Cachet\Enums\IncidentStatusEnum;
 use Cachet\Models\IncidentUpdate;
 use Closure;
 use Illuminate\Contracts\View\View;
@@ -12,7 +13,7 @@ class IncidentUpdateStatus extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct(public IncidentUpdate $update)
+    public function __construct(public IncidentStatusEnum $status)
     {
         //
     }
@@ -23,8 +24,8 @@ class IncidentUpdateStatus extends Component
     public function render(): View|Closure|string
     {
         return view('cachet::components.incident-update-status', [
-            'color' => $this->update->status->getColor(),
-            'icon' => $this->update->status->getIcon(),
+            'color' => $this->status->getColor(),
+            'icon' => $this->status->getIcon(),
         ]);
     }
 }
