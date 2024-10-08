d00699883363910be4feaa9a16e9f16afdd1841c
James Brooks <james@alt-three.com>
2024-10-08 08:28:46 +0100

    Use ToggleButtons component

--- src/Filament/Widgets/Components.php
+++ src/Filament/Widgets/Components.php
@@ -7,7 +7,7 @@ use Cachet\Models\Component;
 use Cachet\Models\ComponentGroup;
 use Filament\Forms\Components\Component as FilamentFormComponent;
 use Filament\Forms\Components\Group;
-use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
@@ -39,6 +39,7 @@ class Components extends Widget implements HasForms
             $components = $componentGroup->components->mapWithKeys(function (Component $component) {
                 return [$component->id => $component->only('status')];
             });
+
             return [$componentGroup->id => ['components' => $components]];
         });
     }
@@ -51,19 +52,16 @@ class Components extends Widget implements HasForms
                 return Section::make($componentGroup->name)
                     ->schema(function () use ($componentGroup) {
                         return
-                            $componentGroup->components->map(function (Component $component) use ($componentGroup) {
-                                return
-                                    Group::make([
-                                        Radio::make($componentGroup->id . '.components.' . $component->id . '.status')
-                                            ->label($component->name)
-                                            ->columns(count(ComponentStatusEnum::cases()))
-                                            ->live()
-                                            ->options(ComponentStatusEnum::class)
-                                            ->afterStateUpdated(function (ComponentStatusEnum $state) use ($component) {
-                                                return $component->update(['status' => $state]);
-                                            })
-                                    ]);
-                            })->toArray();
+                            $componentGroup->components->map(fn (Component $component) => Group::make([
+                                ToggleButtons::make($componentGroup->id . '.components.' . $component->id . '.status')
+                                    ->label($component->name)
+                                    ->inline()
+                                    ->live()
+                                    ->options(ComponentStatusEnum::class)
+                                    ->afterStateUpdated(function (ComponentStatusEnum $state) use ($component) {
+                                        return $component->update(['status' => $state]);
+                                    })
+                            ]))->toArray();
                     })
                     ->collapsed($componentGroup->isCollapsible());
             })->toArray();
@@ -71,7 +69,7 @@ class Components extends Widget implements HasForms
         return $form->schema($schema)->statePath('formData');
     }
 
-    private function loadComponents(HasMany $query)
+    private function loadComponents(HasMany $query): HasMany
     {
         return $query->select(['id', 'component_group_id', 'name', 'status', 'enabled'])
                 ->where('enabled', '=', true);
