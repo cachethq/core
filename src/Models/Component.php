79b153acd8edf5f74f25a63ab645ed626ac2c397
jbrooksuk <jbrooksuk@users.noreply.github.com>
2024-10-08 07:22:20 +0000

    Fix code styling

--- src/Models/Component.php
+++ src/Models/Component.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Component extends Model
