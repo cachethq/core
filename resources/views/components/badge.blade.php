c40f5aa8e8c65f622462b7a9a5a572e72f4585d9
James Brooks <james@alt-three.com>
2024-10-10 19:43:32 +0100

    Improve badge in dark mode

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,3 +1,3 @@
-<x-filament::badge :color="$color" :icon="$icon" class="bg-custom-400 text-custom-900 dark:bg-custom-400/40 dark:text-custom-400">
+<x-filament::badge :color="$color" :icon="$icon" class="bg-custom-400 text-custom-900 dark:text-custom-400">
     {{ $label }}
 </x-filament::badge>
