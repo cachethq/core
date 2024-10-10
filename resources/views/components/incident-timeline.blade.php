d172c3a07b978890a1060055193e6e1a5e8c0603
James Brooks <james@alt-three.com>
2024-10-10 19:58:45 +0100

    Fix pagination dark mode

--- resources/views/components/incident-timeline.blade.php
+++ resources/views/components/incident-timeline.blade.php
@@ -20,13 +20,13 @@
     </div>
 
     <div class="flex justify-between">
-        <a href="{{ route('cachet.status-page', ['from' => $nextPeriodFrom]) }}" class="flex items-center gap-1 border py-2 px-3 rounded-lg text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
+        <a href="{{ route('cachet.status-page', ['from' => $nextPeriodFrom]) }}" class="flex items-center gap-1 border dark:border-zinc-400 py-2 px-3 rounded-lg text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
             <x-heroicon-m-chevron-left class="size-5" />
             {{ __('Previous') }}
         </a>
 
         @if($canPageForward)
-        <a href="{{ route('cachet.status-page', ['from' => $nextPeriodTo]) }}" class="flex items-center gap-1 border py-2 px-3 rounded-lg text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
+        <a href="{{ route('cachet.status-page', ['from' => $nextPeriodTo]) }}" class="flex items-center gap-1 border dark:border-zinc-400 py-2 px-3 rounded-lg text-zinc-500 dark:text-zinc-400 hover:underline text-sm">
             {{ __('Next') }}
             <x-heroicon-m-chevron-right class="size-5" />
         </a>
