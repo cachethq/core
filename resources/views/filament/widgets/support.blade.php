28830dccf0bd05b778c4e37804f2b372f14d5938
James Brooks <james@alt-three.com>
2024-10-10 15:54:28 +0100

    Improve support widget

--- resources/views/filament/widgets/support.blade.php
+++ resources/views/filament/widgets/support.blade.php
@@ -1,13 +1,14 @@
 <x-filament-widgets::widget>
     <x-filament::section :heading="__('Support Cachet')">
-        <p>
+        <p class="text-sm leading-6 text-gray-500 dark:text-gray-400">
             Please consider supporting Cachet via
-            <a href="https://github.com/cachethq/cachet/?sponsor=1" class="underline text-primary-600" target="_blank" rel="nofollow noopener">
+            <x-filament::link href="https://github.com/cachethq/cachet/?sponsor=1" target="_blank" rel="nofollow noopener">
                 GitHub Sponsors
-            </a>.
+            </x-filament::link>.
         </p>
-        <p>
-            Keep up to date with the latest news and releases by following the <a href="https://blog.cachethq.io" class="underline text-primary-600" target="_blank" rel="nofollow noopener">Cachet blog</a>.
+        <p class="text-sm leading-6 text-gray-500 dark:text-gray-400">
+            Keep up to date with the latest news and releases by following the <x-filament::link href="https://blog.cachethq.io" target="_blank" rel="nofollow noopener">Cachet blog</x-filament::link>.
         </p>
     </x-filament::section>
 </x-filament-widgets::widget>
+
