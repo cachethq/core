9b4fff141289ba6a272e07375f80dbc5de7c60dc
Martín Gómez <martin@raiolanetworks.es>
2024-10-31 15:59:57 +0100

    feat: Improve about section with site name

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,6 +1,6 @@
 @if ($about !== '')
 <div>
-    <h2 class="text-3xl font-semibold">{{ __('About This Site') }}</h2>
+    <h1 class="text-3xl font-semibold">{{ {{ $title }} }}</h1>
     <div class="prose-sm md:prose prose-zinc mt-1 dark:prose-invert prose-a:text-primary-500 prose-a:underline">
         {!! $about !!}
     </div>
