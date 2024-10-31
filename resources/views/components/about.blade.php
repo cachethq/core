07b2ee17717650b4ab8e063a19c84e9605099aa5
Martín Gómez <6207345+soymgomez@users.noreply.github.com>
2024-10-31 17:10:10 +0100

    Update resources/views/components/about.blade.php
    Co-authored-by: James Brooks <james@alt-three.com>

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,6 +1,6 @@
 @if ($about !== '')
 <div>
-    <h1 class="text-3xl font-semibold">{{ {{ $title }} }}</h1>
+    <h1 class="text-3xl font-semibold">{{ $title }}</h1>
     <div class="prose-sm md:prose prose-zinc mt-1 dark:prose-invert prose-a:text-primary-500 prose-a:underline">
         {!! $about !!}
     </div>
