21c1b8997338f88561b102b2a1e9b970899a8899
James Brooks <james@alt-three.com>
2024-10-31 15:53:49 +0000

    wip

---
+++ src/Support/View/RenderHook.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cachet\Support\View;
+
+class RenderHook
+{
+    const STATUS_PAGE_NAVIGATION_AFTER = 'cachet::status-page.navigation.after';
+
+    const STATUS_PAGE_NAVIGATION_BEFORE = 'cachet::status-page.navigation.before';
+
+    const STATUS_PAGE_COMPONENTS_AFTER = 'cachet::status-page.components.after';
+
+    const STATUS_PAGE_COMPONENTS_BEFORE = 'cachet::status-page.components.before';
+
+    const FOOTER = 'cachet::footer';
+}
