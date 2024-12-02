c56f0cd1841b91eb358ec2359c5941ac97a617cd
James Brooks <james@alt-three.com>
2024-12-02 11:32:34 +0000

    Add logout button to status page

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -13,8 +13,13 @@
     @if ($dashboardLoginLink)
     <div class="flex items-center gap-2.5 sm:gap-5">
         <a href="{{ Cachet\Cachet::dashboardPath() }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 sm:text-base">{{ __('Dashboard') }}</a>
-        {{-- Condition Button. --}}
-{{--        <a href="{{ route('cachet.dashboard.index') }}" class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 sm:text-base">Logout</a>--}}
+        @auth
+        {{-- TODO: This form sucks... --}}
+        <form action="{{ \Cachet\Cachet::dashboardPath() }}/logout" method="POST">
+            @csrf
+            <button class="text-sm font-medium text-zinc-800 transition hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300 sm:text-base">Logout</button>
+        </form>
+        @endauth
         {{-- Conditional Button. --}}
 {{--        <a href="{{ route('cachet.dashboard.index') }}" class="rounded bg-primary-500 px-2 py-1 text-sm font-semibold text-zinc-800 dark:text-zinc-400 transition hover:bg-primary-600">Subscribers</a>--}}
     </div>
