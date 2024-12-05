a4f526d4e778677be240682bd8c19ef95df5473d
James Brooks <james@alt-three.com>
2024-12-05 17:49:37 +0000

    Make User resource mixin of Authenticatable

--- src/Http/Resources/User.php
+++ src/Http/Resources/User.php
@@ -5,12 +5,15 @@ namespace Cachet\Http\Resources;
 use Illuminate\Http\Request;
 use TiMacDonald\JsonApi\JsonApiResource;
 
+/**
+ * @mixin \Illuminate\Contracts\Auth\Authenticatable
+ */
 class User extends JsonApiResource
 {
     public function toAttributes(Request $request): array
     {
         return [
-            'id' => $this->id,
+            'id' => $this->getAuthIdentifier(),
         ];
     }
 }
