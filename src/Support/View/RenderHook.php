d1e1bd156c3c069d5d0fa3ab9fd16f8d9b0021d5
James Brooks <james@alt-three.com>
2024-12-22 12:18:14 +0000

    Add more hooks

--- src/Support/View/RenderHook.php
+++ src/Support/View/RenderHook.php
@@ -12,5 +12,23 @@ class RenderHook
 
     const STATUS_PAGE_COMPONENTS_BEFORE = 'cachet::status-page.components.before';
 
+    const STATUS_PAGE_COMPONENT_GROUPS_AFTER = 'cachet::status-page.component-groups.after';
+
+    const STATUS_PAGE_COMPONENT_GROUPS_BEFORE = 'cachet::status-page.component-groups.before';
+
+    const STATUS_PAGE_INCIDENTS_AFTER = 'cachet::status-page.incidents.after';
+
+    const STATUS_PAGE_INCIDENTS_BEFORE = 'cachet::status-page.incidents.before';
+
+    const STATUS_PAGE_SCHEDULES_AFTER = 'cachet::status-page.schedules.after';
+
+    const STATUS_PAGE_SCHEDULES_BEFORE = 'cachet::status-page.schedules.before';
+
+    const STATUS_PAGE_BANNER = 'cachet::status-page.banner';
+
+    const STATUS_PAGE_METRICS_AFTER = 'cachet::status-page.metrics.after';
+
+    const STATUS_PAGE_METRICS_BEFORE = 'cachet::status-page.metrics.before';
+
     const FOOTER = 'cachet::footer';
 }
