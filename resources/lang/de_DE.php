6b43f646f8f5763c2a5289b13adeaa1dce57bf6f
driverspy <59536815+driverspy@users.noreply.github.com>
2024-12-30 08:05:47 +0100

    chore: added a german php translation (as template/draft)

---
+++ resources/lang/de_DE.php
@@ -0,0 +1,9 @@
+<?php
+// JUST A DRAFT
+return [
+  'admin' => 'Admin',
+  'always_hidden' => 'Immer verborgen',
+  'always_collapsed' => 'Immer eingeklappt',
+  'always_expanded' => 'Immer ausgeklappt',
+  // ...
+];
