2eca7b3c97639476215241c8350bbe1d5c28c418
James Brooks <james@alt-three.com>
2025-01-02 19:43:29 +0000

    Fix en_GB customisation

--- resources/lang/en_GB/settings.php
+++ resources/lang/en_GB/settings.php
@@ -3,7 +3,7 @@
 return [
     'manage_theme' => [
         'status_page_accent' => [
-            'description' => 'Customize the accent colour of your status page. Cachet can automatically select a matching base colour.',
+            'description' => 'Customise the accent colour of your status page. Cachet can automatically select a matching base colour.',
             'accent_color_label' => 'Accent Colour',
             'accent_content_label' => 'Base Colour',
         ]
