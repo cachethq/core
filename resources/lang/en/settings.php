46996dd85604ddaec6661a7d294bb0cd0677e48f
Dan Johnson <dan@try.be>
2025-01-02 18:56:44 +0000

    feat: add language files

---
+++ resources/lang/en/settings.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'manage_cachet' => [
+        'site_name_label' => 'Site Name',
+        'about_this_site_label' => 'About This Site',
+        'timezone_label' => 'Timezone',
+        'incident_days_label' => 'Incident Days',
+        'major_outage_threshold_label' => 'Major Outage Threshold',
+        'refresh_rate_label' => 'Automatically Refresh Page',
+        'refresh_rate_label_input_suffix_seconds' => 'seconds',
+        'toggles' => [
+            'support_cachet' => 'Support Cachet',
+            'show_timezone' => 'Show Timezone',
+            'show_dashboard_link' => 'Show Dashboard Link',
+            'display_graphs' => 'Display Graphs',
+            'only_show_disrupted_days' => 'Only Show Disrupted Days',
+        ],
+    ],
+    'manage_customization' => [
+        'header_label' => 'Custom Header HTML',
+        'footer_label' => 'Custom Footer HTML',
+        'stylesheet_label' => 'Custom CSS',
+    ],
+    'manage_theme' => [
+        'app_banner_label' => 'Banner Image',
+        'status_page_accent' => [
+            'heading' => 'Status Page Accent',
+            'description' => 'Customize the accent color of your status page. Cachet can automatically select a matching base color.',
+            'accent_color_label' => 'Accent Color',
+            'accent_content_label' => 'Base Color',
+            'accent_pairing_label' => 'Accent Pairing',
+        ]
+    ]
+];
