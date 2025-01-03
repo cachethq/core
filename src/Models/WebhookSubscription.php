520b4768c7963c79583b936b1aee8732553fb92b
Dan Johnson <dan@try.be>
2025-01-03 21:57:59 +0000

    chore: create model for subscription

---
+++ src/Models/WebhookSubscription.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Cachet\Models;
+
+use Cachet\Enums\WebhookEventEnum;
+use Illuminate\Database\Eloquent\Casts\AsEnumCollection;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\WebhookServer\WebhookCall;
+
+class WebhookSubscription extends Model
+{
+    protected $fillable = [
+        'url',
+        'secret',
+        'description',
+        'send_all_events',
+        'selected_events',
+    ];
+
+    protected $casts = [
+        'selected_events' => AsEnumCollection::class . ':' . WebhookEventEnum::class,
+    ];
+
+    public function scopeWhereEvent(WebhookEventEnum $event)
+    {
+        return $this->where('send_all_events', true)
+            ->orWhereJsonContains('selected_events', $event->value);
+    }
+
+    public function makeCall(WebhookEventEnum $event, array $payload): WebhookCall
+    {
+        return WebhookCall::create()
+            ->url($this->url)
+            ->payload($payload)
+            ->meta([
+                'subscription_id' => $this->getKey(),
+                'event' => $event->value,
+            ])
+            ->useSecret($this->secret);
+    }
+}
