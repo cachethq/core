617b2c6b101901c0443daca3ca99eb1c12b716b9
Dan Johnson <dan@try.be>
2025-01-16 23:36:41 +0000

    chore: middleware for setting locale

---
+++ src/Http/Middleware/SetAppLocale.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cachet\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SetAppLocale
+{
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var \Cachet\Models\User */
+        $user = $request->user();
+
+        if ($user) {
+            app()->setLocale($user->preferredLocale() ?? $request->getPreferredLanguage(
+                array_keys(config('cachet.supported_locales'))
+            ));
+        }
+
+        return $next($request);
+    }
+}
