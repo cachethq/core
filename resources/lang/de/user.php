0f095e5066efc9d127fed5943cb44dc8739db5e4
Kev <59536815+driverspy@users.noreply.github.com>
2025-01-17 07:21:25 +0100

    Update user.php (lang: german) - 9682337

--- resources/lang/de/user.php
+++ resources/lang/de/user.php
@@ -5,4 +5,23 @@ return [
         'admin' => 'Admin',
         'user' => 'Benutzer',
     ],
+    'resource_label' => 'Benutzer|Benutzer',
+    'list' => [
+        'headers' => [
+            'name' => 'Name',
+            'email' => 'Email-Addresse',
+            'email_verified_at' => 'Email verifiziert am',
+            'is_admin' => 'Ist Administrator?',
+        ],
+        'actions' => [
+            'verify_email' => 'Email verifizieren',
+        ],
+    ],
+    'form' => [
+        'name_label' => 'Name',
+        'email_label' => 'Email-Addresse',
+        'password_label' => 'Passwort',
+        'password_confirmation_label' => 'Passwort bestätigen',
+        'is_admin_label' => 'Administrator',
+    ],
 ];
