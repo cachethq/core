28ae3f196ea15060ca7cf1b379e0dff851dbb335
Kev <59536815+driverspy@users.noreply.github.com>
2025-01-20 17:33:21 +0100

    Multiple updates for german translation files (#206)

---
+++ resources/lang/de_DE/integrations.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'oh_dear' => [
+        'import_feed_button' => 'Feed importieren',
+        'status_page_url_label' => 'Oh Dear Statusseiten-URL',
+        'status_page_url_helper' => 'Trage die URL deiner Oh Dear Statusseite ein (eg. https://status.example.com).',
+        'import_sites_as_components_label' => 'Importiere Seiten als Komponenten',
+        'import_sites_as_components_helper' => 'Seiten, welche bei Oh Dear konfiguriert wurden, werden als Komponenten in Cachet hinzugefügt.',
+        'component_group_label' => 'Komponentengruppe',
+        'component_group_helper' => 'Die Komponentengruppe, der importierte Komponenten zugewiesen werden sollen.',
+        'import_incidents_label' => 'Vorfälle importieren',
+        'import_incidents_helper' => 'Aktuelle Vorfälle von Oh Dear werden als Vorfälle in Cachet importiert.',
+        'provided_url_invalid' => 'Die angegebene URL ist kein gültiger Endpunkt der Oh Dear-Statusseite.',
+        'imported_successfully' => 'Oh Dear Import erfolgreich.',
+    ],
+];
