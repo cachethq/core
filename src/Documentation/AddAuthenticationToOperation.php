c36badb05efd7a43e208acf61ec7f8b1629cb0a0
Roman Lytvynenko <litvinenko95@gmail.com>
2025-01-20 12:56:04 +0200

    proper auth docs implementation

--- src/Documentation/AddAuthenticationToOperation.php
+++ src/Documentation/AddAuthenticationToOperation.php
@@ -5,14 +5,15 @@ namespace Cachet\Documentation;
 use Dedoc\Scramble\Extensions\OperationExtension;
 use Dedoc\Scramble\Support\Generator\Operation;
 use Dedoc\Scramble\Support\RouteInfo;
+use Illuminate\Support\Str;
 
 class AddAuthenticationToOperation extends OperationExtension
 {
     public function handle(Operation $operation, RouteInfo $routeInfo)
     {
-        // @todo find out if a route is protected by auth by looking at route's middleware to avoid manually annotating methods
+        $hasAuthMiddleware = collect($routeInfo->route->gatherMiddleware())->contains(fn ($m) => Str::startsWith($m, 'auth:'));
 
-        if (in_array('GET', $routeInfo->route->methods())) {
+        if (! $hasAuthMiddleware) {
             $operation->addSecurity([]);
         }
     }
