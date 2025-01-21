692656697f23087f2538a77ae0283f281766cd6e
Joel Butcher <joelbutcher@users.noreply.github.com>
2025-01-21 11:21:44 +0000

    Playing with form submission

--- resources/lang/en/subscriptions.php
+++ resources/lang/en/subscriptions.php
@@ -3,8 +3,17 @@
 return [
     'button_label' => 'Subscribe to updates',
     'create' => [
-        'page_heading' => 'Subscribe to Updates',
-        'email_label' => 'Email Address',
-        'email_placeholder' => 'Enter your email address',
+        'heading' => 'Subscribe to Updates',
+        'form' => [
+            'email' => [
+                'label' => 'Email Address',
+                'placeholder' => 'Enter your email address',
+            ],
+            'phone' => [
+                'label' => 'Phone Number',
+                'placeholder' => 'Enter your phone number',
+            ],
+            'submit_label' => 'Subscribe',
+        ],
     ],
 ];
--- src/Filament/Components/Subscriptions/CreateSubscriptionForm.php
+++ src/Filament/Components/Subscriptions/CreateSubscriptionForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Cachet\Filament\Components\Subscriptions;
 
+use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Forms\Concerns\InteractsWithForms;
@@ -12,33 +13,62 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Livewire\Component;
 
+/**
+ * @property-read Form $form
+ */
 class CreateSubscriptionForm extends Component implements HasActions, HasForms
 {
     use InteractsWithActions, InteractsWithForms;
 
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
     public function form(Form $form): Form
     {
         return $form
+            ->statePath('data')
             ->schema([
-                Forms\Components\Section::make(heading: __('cachet::subscriptions.create.page_heading'))
+                Forms\Components\Section::make(heading: __('cachet::subscriptions.create.heading'))
                     ->columns()
                     ->schema([
-                        Forms\Components\TextInput::make('email')
-                            ->label(__('cachet::subscriptions.create.email_label'))
-                            ->placeholder(__('cachet::subscriptions.create.email_placeholder'))
+                        Forms\Components\TextInput::make('email_address')
+                            ->label(__('cachet::subscriptions.create.form.email.label'))
+                            ->placeholder(__('cachet::subscriptions.create.form.email.placeholder'))
+                            ->required()
                             ->email()
                             ->maxLength(255)
-                            ->required(),
+                            ->autofocus()
+                            ->autocomplete('email'),
+                        Forms\Components\TextInput::make('phone_number')
+                            ->label(__('cachet::subscriptions.create.form.phone.label'))
+                            ->placeholder(__('cachet::subscriptions.create.form.phone.placeholder'))
+                            ->tel(),
                     ]),
             ]);
+    }
+
+    public function store(): void
+    {
+        $state = $this->form->getState();
+
+        dd($state);
     }
 
     public function render(): string
     {
         return <<<'blade'
-            <div>
+            <form wire:submit.prevent="store" class="space-y-6 max-w-6xl">
                 {{ $this->form }}
-            </div>
+                
+                <div class="flex justify-end space-x-2">
+                    <x-filament::button size="lg" color="gray" tag="a" :href="route('cachet.status-page')">Cancel</x-filament::button>
+                    <x-filament::button size="lg" type="submit">Subscribe</x-filament::button>
+                </div>
+            </form>
         blade;
     }
 }
