1b32bc35889e9e150f816371eea5c2ced9b77fd1
James Brooks <james@alt-three.com>
2025-01-21 21:20:28 +0000

    Support the `HTTP_REMOTE_USER` header (#208)

---
+++ src/Http/Middleware/AuthenticateRemoteUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cachet\Http\Middleware;
+
+use Cachet\Cachet;
+use Cachet\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateRemoteUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($remoteUser = $request->headers->get('REMOTE_USER')) {
+            $userModel = Cachet::userModel();
+            /** @var User|null $user */
+            $user = $userModel::query()->where('email', $remoteUser)->first();
+
+            if ($user !== null) {
+                auth()->login($user);
+            }
+        }
+
+        return $next($request);
+    }
+}
