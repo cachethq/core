40c4224c928e2a525953f2f74bd744185a3aa559
Kev <59536815+driverspy@users.noreply.github.com>
2025-01-21 15:34:41 +0100

    Added nl_NL (Netherlands) as tanslation language

---
+++ resources/lang/nl_NL/navigation.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'settings' => [
+        'label' => 'Instellingen',
+        'items' => [
+            'manage_cachet' => 'Beheer Cachet',
+            'manage_customization' => 'Beheer maatwerk',
+            'manage_theme' => 'Weergave beheren',
+            'manage_api_keys' => 'API-sleutels beheren',
+            'manage_webhooks' => 'Webhooks beheren',
+        ],
+    ],
+
+    'integrations' => [
+        'label' => 'Integraties',
+        'items' => [
+            'oh_dear' => 'Oh Dear',
+        ],
+    ],
+    'resources' => [
+        'label' => 'Bronnen',
+        'items' => [
+            'status_page' => 'Statuspagina',
+            'documentation' => 'Documentatie',
+            'discord' => 'Word lid van Discord',
+            'sponsor' => 'Ondersteuning Cachet',
+        ],
+    ],
+];
---
+++ resources/lang/nl_NL/system_status.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'operational' => 'Alle systemen zijn functioneel.',
+    'partial_outage' => 'Er zijn problemen met sommige systemen.',
+    'major_outage' => 'Sommige systemen ondervinden aanzienlijke problemen.',
+];
