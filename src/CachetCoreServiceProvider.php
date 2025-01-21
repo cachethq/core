0d8df427b77f227da5d402fd586b0ab640a73f25
James Brooks <james@alt-three.com>
2025-01-21 20:59:58 +0000

    Work on component monitoring

--- src/CachetCoreServiceProvider.php
+++ src/CachetCoreServiceProvider.php
@@ -169,6 +169,7 @@ class CachetCoreServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
+                Commands\CheckComponentsCommand::class,
                 Commands\MakeUserCommand::class,
                 Commands\SendBeaconCommand::class,
                 Commands\VersionCommand::class,
