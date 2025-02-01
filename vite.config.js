3a415d42ebe6181ffe648622a90aec4a38a272bd
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2025-02-01 16:10:06 -0500

    wip: upgrade to tailwind v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   build: {
     cssMinify: 'lightningcss'
   },
   plugins: [
+    tailwindcss(),
     laravel({
       input: [
         'resources/css/cachet.css',
