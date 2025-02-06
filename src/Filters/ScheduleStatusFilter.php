aba41ac45d86a89ba1e879985f861d2ec12d322f
Sven <sven@vioms.com>
2025-02-06 19:38:01 +0100

    Fixing PHPStan

--- src/Filters/ScheduleStatusFilter.php
+++ src/Filters/ScheduleStatusFilter.php
@@ -3,11 +3,18 @@
 namespace Cachet\Filters;
 
 use Cachet\Enums\ScheduleStatusEnum;
+use Cachet\QueryBuilders\ScheduleBuilder;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
 
 class ScheduleStatusFilter implements Filter
 {
+    /**
+     * @param  ScheduleBuilder  $query
+     * @param $value
+     * @param  string  $property
+     * @return void
+     */
     public function __invoke(Builder $query, $value, string $property)
     {
         match($value) {
@@ -17,6 +24,7 @@ class ScheduleStatusFilter implements Filter
             ScheduleStatusEnum::in_progress->name => $query->inProgress(),
             (string) ScheduleStatusEnum::upcoming->value,
             ScheduleStatusEnum::upcoming->name => $query->inTheFuture(),
+            default => null,
         };
     }
 
---
+++ src/QueryBuilders/ScheduleBuilder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Cachet\QueryBuilders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @inheritDoc
+ * @method static \Cachet\QueryBuilders\ScheduleBuilder incomplete()
+ * @method static \Cachet\QueryBuilders\ScheduleBuilder inProgress()
+ * @method static \Cachet\QueryBuilders\ScheduleBuilder inTheFuture()
+ * @method static \Cachet\QueryBuilders\ScheduleBuilder inThePast()
+ */
+class ScheduleBuilder extends Builder
+{
+    /**
+     * Scope schedules that are incomplete.
+     *
+     * @return self
+     */
+    public function incomplete(): self
+    {
+        $this->whereDate('completed_at', '>=', Carbon::now())
+            ->orWhereNull('completed_at');
+        return $this;
+    }
+
+    /**
+     * Scope schedules that are in progress.
+     *
+     * @return self
+     */
+    public function inProgress(): self
+    {
+        $this->whereDate('scheduled_at', '<=', Carbon::now())
+            ->where(function (ScheduleBuilder $query) {
+                $query->whereDate('completed_at', '>=', Carbon::now())
+                    ->orWhereNull('completed_at');
+            });
+        return $this;
+    }
+
+    /**
+     * Scopes schedules to those in the future.
+     *
+     * @return self
+     */
+    public function inTheFuture(): self
+    {
+        $this->whereDate('scheduled_at', '>=', Carbon::now());
+        return $this;
+    }
+
+    /**
+     * Scopes schedules to those scheduled in the past.
+     *
+     * @return self
+     */
+    public function inThePast(): self
+    {
+        $this->where('completed_at', '<=', Carbon::now())
+            ->whereNotNull('completed_at');
+        return $this;
+    }
+}
