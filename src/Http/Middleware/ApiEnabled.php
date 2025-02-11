f4544963fcadc6999bff482157133146d2330cba
Sven <sven@vioms.com>
2025-02-11 17:17:46 +0100

    Api Enabled should not have to be extended from Auth

--- src/Http/Middleware/ApiEnabled.php
+++ src/Http/Middleware/ApiEnabled.php
@@ -4,23 +4,19 @@ namespace Cachet\Http\Middleware;
 
 use Cachet\Settings\AppSettings;
 use Closure;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Auth\Middleware\Authenticate as BaseAuthenticationMiddleware;
 use Illuminate\Http\Request;
 
-class ApiEnabled extends BaseAuthenticationMiddleware
+class ApiEnabled
 {
     /**
      * Handle an incoming request.
      *
      * @param  Request  $request
      * @param  Closure(Request):mixed  $next
-     * @param  string  ...$guards
      * @return mixed
      *
-     * @throws AuthenticationException
      */
-    public function handle($request, Closure $next, ...$guards)
+    public function handle($request, Closure $next)
     {
         if (!AppSettings::getOrDefault('api_enabled', true)) {
             abort(404);
