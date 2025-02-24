8743d2e34bbbd538d6250124b6061120bd2870d1
James Brooks <james@alt-three.com>
2025-02-24 11:54:11 +0000

    Show latest news in dashboard

--- resources/lang/en/cachet.php
+++ resources/lang/en/cachet.php
@@ -7,6 +7,11 @@ return [
         'keep_up_to_date' => 'Keep up to date with the latest news and releases by following the *Cachet blog*.',
         'work_in_progress_text' => 'Cachet is under active development. Things are still subject to change.',
     ],
+    'feed' => [
+        'section_heading' => 'Latest Blog Posts',
+        'empty' => 'No blog posts were found. Check *the blog* for further information.',
+        'posted_at' => 'Posted :date'
+    ],
     'powered_by' => 'Powered by',
     'open_source_status_page' => 'The open-source status page.',
     'all_times_shown_in' => 'All times are shown in *:timezone*.',
