4b6a14ed2018deb1539be4dc539a53912e733e86
Bastien Wermeille <bastien.wermeille@gmail.com>
2025-03-25 15:22:40 +0100

    Complete french traductions

--- resources/lang/fr/cachet.php
+++ resources/lang/fr/cachet.php
@@ -7,6 +7,11 @@ return [
         'keep_up_to_date' => 'Restez informé des dernières nouvelles et mises à jour en suivant le *blog de Cachet*.',
         'work_in_progress_text' => 'Cachet est en cours de développement actif. Des modifications peuvent encore survenir.',
     ],
+    'feed' => [
+        'section_heading' => 'Derniers articles',
+        'empty' => 'Aucun article trouvé. Consultez *le blog* pour plus d’informations.',
+        'posted_at' => 'Posté le :date',
+    ],
     'powered_by' => 'Propulsé par',
     'open_source_status_page' => 'La page de statut open-source.',
     'all_times_shown_in' => 'Toutes les heures sont affichées en *:timezone*.',
