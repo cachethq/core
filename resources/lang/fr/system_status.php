7d0fafde541d1cdbc3a354ca480d5a46c347de96
THIPHONET Stephane DGAC/DTI <stephane.thiphonet@aviation-civile.gouv.fr>
2025-06-27 15:29:09 +0200

    update system_status fr locale

--- resources/lang/fr/system_status.php
+++ resources/lang/fr/system_status.php
@@ -4,5 +4,5 @@ return [
     'operational' => 'Tous les systèmes sont opérationnels.',
     'partial_outage' => 'Certains systèmes rencontrent des problèmes.',
     'major_outage' => 'Certains systèmes rencontrent des problèmes majeurs.',
-    'major_outage' => 'Certains systèmes sont en maintenance.',
+    'under_maintenance' => 'Certains systèmes sont en maintenance.',
 ];
