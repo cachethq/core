8e206207343d02ee5a783aaa289924d056481d78
Ashwin Chandran <ashwin.chandran@heigit.org>
2025-11-17 19:40:48 +0100

    Fix Component resource pivot access with isset checks

--- src/Http/Resources/Component.php
+++ src/Http/Resources/Component.php
@@ -30,12 +30,12 @@ class Component extends JsonApiResource
                 'human' => $this->updated_at?->diffForHumans(),
                 'string' => $this->updated_at?->toDateTimeString(),
             ],
-            'pivot' => $this->when($this->pivot, function () {
+            'pivot' => $this->when(isset($this->pivot) && isset($this->pivot->component_status), function () {
                 return [
-                    'component_status' => $this->pivot->component_status ? [
+                    'component_status' => [
                         'human' => $this->pivot->component_status->getLabel(),
                         'value' => $this->pivot->component_status->value,
-                    ] : null,
+                    ],
                 ];
             }),
         ];
