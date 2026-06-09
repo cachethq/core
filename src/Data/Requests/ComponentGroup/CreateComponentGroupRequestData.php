65621230a3d00e155253b2a4553aeed878fe34b3
Ashwin Chandran <ashwin.chandran@heigit.org>
2026-06-09 08:35:33 +0200

    Allow setting component-group collapsed state via API

--- src/Data/Requests/ComponentGroup/CreateComponentGroupRequestData.php
+++ src/Data/Requests/ComponentGroup/CreateComponentGroupRequestData.php
@@ -3,6 +3,7 @@
 namespace Cachet\Data\Requests\ComponentGroup;
 
 use Cachet\Data\BaseData;
+use Cachet\Enums\ComponentGroupVisibilityEnum;
 use Cachet\Enums\ResourceVisibilityEnum;
 use Illuminate\Validation\Rule;
 use Spatie\LaravelData\Support\Validation\ValidationContext;
@@ -13,6 +14,7 @@ final class CreateComponentGroupRequestData extends BaseData
         public readonly string $name,
         public readonly ?int $order = null,
         public readonly ?ResourceVisibilityEnum $visible = null,
+        public readonly ?ComponentGroupVisibilityEnum $collapsed = null,
         public readonly ?array $components = null,
     ) {}
 
@@ -22,8 +24,24 @@ final class CreateComponentGroupRequestData extends BaseData
             'name' => ['required', 'string', 'max:255'],
             'order' => ['int', 'min:0'],
             'visible' => ['bool'],
+            'collapsed' => [Rule::enum(ComponentGroupVisibilityEnum::class)],
             'components' => ['array'],
             'components.*' => ['int', 'min:0', Rule::exists('components', 'id')],
+        ];
+    }
+
+    public function bodyParameters(): array
+    {
+        return [
+            'collapsed' => [
+                'description' => 'The collapsed state of the component group on the status page.',
+                'example' => '0',
+                'required' => false,
+                'schema' => [
+                    'type' => 'integer',
+                    'enum' => ComponentGroupVisibilityEnum::cases(),
+                ],
+            ],
         ];
     }
 }
